a47587a5a98e2211d7f72f7f931afc20ddde2926
Diego Ampuero <diego@ampuero.it>
2026-04-09 16:02:41 +0200

    fix on the settings

--- quicknotes/settings.py
+++ quicknotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*q^xii531v=xa$lj5w%^hhr++tq=i42v&&^_xi&nj76ru^uu=4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
